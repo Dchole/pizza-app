124985a17886e977e2bd34e7aef5eadac848fb4d
Dchole <derekoware47@gmail.com>
2021-06-30 17:46:48 +0000

    Configuration

---
+++ src/components/Header.tsx
@@ -0,0 +1,12 @@
+import AppBar from "@material-ui/core/AppBar"
+import Toolbar from "@material-ui/core/Toolbar"
+
+const Header = () => {
+  return (
+    <AppBar variant="outlined">
+      <Toolbar>Lorem</Toolbar>
+    </AppBar>
+  )
+}
+
+export default Header
