e82c9fa63b63e22df93acd6ee56591d4f0c06a85
Dchole <derekoware47@gmail.com>
2021-06-30 21:14:38 +0000

    postcss

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,11 @@
+import styles from "./Header.module.css"
+
+const Header = () => {
+  return (
+    <header className={styles.root}>
+      <div className="child"></div>
+    </header>
+  )
+}
+
+export default Header
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,12 @@
 import Header from "@/components/Header"
+import Head from "next/head"
 
 export default function Home() {
   return (
     <div>
+      <Head>
+        <title>Home</title>
+      </Head>
       <Header />
     </div>
   )
