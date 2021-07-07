a72d3f1fb01e400ae29ece0513a7156e4c257547
Dchole <derekoware47@gmail.com>
2021-07-07 15:42:39 +0000

    performance boost

--- src/components/PageBackdrop/useStyles.ts
+++ src/components/PageBackdrop/useStyles.ts
@@ -16,7 +16,7 @@ export const useStyles = makeStyles(theme =>
       zIndex: 0
     },
     paper: {
-      padding: theme.spacing(2),
+      padding: theme.spacing(2, 2, 4),
       boxShadow: `0px -2px 3px 0px rgb(0 0 0 / 12%),
                   0px -1px 1px 0px rgb(0 0 0 / 14%), 
                   0px -1px 1px -1px rgb(0 0 0 / 20%)`,
