a5b2efa6fcad2fe697a898c6cbc6bb9f6f258da6
Dchole <derekoware47@gmail.com>
2021-07-15 18:47:29 +0000

    production-alpha v1

--- src/cms.ts
+++ src/cms.ts
@@ -1,7 +1,7 @@
 export const cmsLinks = {
   hostname:
     process.env.NODE_ENV === "production"
-      ? "https://moshood-pizza.herokuapp.com/" // Strapi production host
+      ? "https://moshood-pizza.herokuapp.com" // Strapi production host
       : "http://localhost:1337", // Strapi development or local host
   get api() {
     return this.hostname + "/graphql" // Strapi GraphQL endpoint
