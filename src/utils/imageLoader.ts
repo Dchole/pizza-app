df24c43455fea5aebc1e3ea93cbb1b9ae72ed3cb
Dchole <derekoware47@gmail.com>
2021-07-20 13:20:06 +0000

    loading images from cloudinary

--- src/utils/imageLoader.ts
+++ src/utils/imageLoader.ts
@@ -1,5 +1,3 @@
-import { cmsLinks } from "cms"
 import { ImageLoaderProps } from "next/image"
 
-export const loader = ({ src }: ImageLoaderProps) =>
-  `${cmsLinks.hostname}${src}`
+export const loader = ({ src }: ImageLoaderProps) => src
